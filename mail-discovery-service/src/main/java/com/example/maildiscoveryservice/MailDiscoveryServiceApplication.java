bdd9954a12607fc1106576bfee199b4d439801b5
Kamil Niedzielak <kamil.niedzielak.ctr@sabre.com>
2021-10-06 12:05:04 +0200

    Initial commit

---
+++ mail-discovery-service/src/main/java/com/example/maildiscoveryservice/MailDiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.maildiscoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailDiscoveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailDiscoveryServiceApplication.class, args);
+    }
+
+}
---
+++ mail-discovery-service/src/test/java/com/example/maildiscoveryservice/MailDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maildiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailDiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
